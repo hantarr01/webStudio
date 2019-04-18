4c45b451ca81dbafa7ecb435c67bd2da8876da7f
hantarr01 <thunderbolt655@mail.ru>
2019-04-18 11:08:10 +0300

    0.1.5

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-	$(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+ $(document).ready(function(){
+ $('.slide').bxSlider();
+ });
+
+ $('.slide').bxSlider({
+mode: 'fade',
+randomStart: true,
+controls: false,
+auto: true,
+speed: 1000,
+pause: 4000,
+pager: false,
+autoControls: false
+});
