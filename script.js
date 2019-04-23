7d18eeb08769a7955692129a245d49e6a2697163
Skorpzmey <ilyushin.51072@gmail.com>
2019-04-23 16:26:27 +0300

    site v0.8.5

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 	$(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+var ele = document.getElementById('menu-min');
+
+
+
+if ($(window).width() < 700) {
+	var element = document.getElementById('menu');
+	element.remove();
+	$(ele).css('display', 'block');
+} else {
+	console.log('false');
+}
