319872b698797d601e85b21b065858f23bf1a48c
Lagrek <rain.demorser@gmail.com>
2019-04-24 16:44:11 +0300

    Engame 0.9

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+
+	$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+var ele = document.getElementById('menu-min');
+
+
+
+if ($(window).width() < 700) {
+	var element = document.getElementById('menu');
+	element.remove();
+	$(ele).css('display', 'block');
+} else {
+	console.log('false');
+}
+
 var slideEl = $(".slide--parent");
 
 slideEl.flickity({
 	imagesLoaded: true,
 	wrapAround: true,
-	autoPlay: 9000,
-	pauseAutoPlayOnHover: false
+	autoPlay: 8000,
+	prevNextButtons:false,
 });
+
